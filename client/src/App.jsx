8cdcce9f2807f8e5ff79e64476989fce8a0dbdcb
Musiemhy <Musiemh2001@gmail.com>
2024-09-13 23:29:28 +0300

    feat: add basic routing for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "../src/pages/HomePage/HomePage";
+import BudgetPage from "../src/pages/BudgetPage/BudgetPage";
+import TransactionPage from "../src/pages/TransactionPage/TransactionPage";
+import SettingPage from "../src/pages/SettingPage/SettingPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/budget" element={<BudgetPage />} />
+        <Route path="/transaction" element={<TransactionPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/SettingPage/SettingPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SettingPage.scss";
+
+function SettingPage() {
+  return <div></div>;
+}
+
+export default SettingPage;
